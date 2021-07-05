5967aba11fc552674f7501fc99eafeadbd46c00e
Ganesh Nayanajith <70511207+ganeshshoutoutlabs@users.noreply.github.com>
2021-07-05 19:11:38 +0530

    first commit with initial project setup

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const loggerMorgan = require('morgan');
+
+const logger = require('./lib/logger');
+const config = require('./lib/config');
+const log = logger(config.logger);
+
+const BASE_PATH = config.api.server_base_path;
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(loggerMorgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(`${BASE_PATH}/`, indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
